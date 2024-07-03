8f29634e1e59842f96dce0c35b404fc30fb19c7b
TestUser <testuser@example.com>
2024-07-03 21:35:00 +0530

    added index.html

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-# Example script to start your web server (replace with your actual server start command)
-sudo systemctl start httpd  # Replace with your server's start command
+sudo systemctl start httpd
