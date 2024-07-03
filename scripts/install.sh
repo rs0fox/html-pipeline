b2723217d524cf5fe475c2bb38444fde1b9c431b
TestUser <testuser@example.com>
2024-07-03 21:06:40 +0530

    added index.html

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@ version: 0.0
 os: linux
 files:
   - source: /
-    destination: /var/www/html  # Replace with your application's destination directory
+    destination: /var/www/html
 permissions:
   - object: /var/www/html
     pattern: "**"
@@ -10,6 +10,11 @@ permissions:
     group: apache  # Replace with appropriate group
 
 hooks:
+  BeforeInstall:
+    - location: scripts/install.sh
+      timeout: 300
+      runas: root
+
   ApplicationStop:
     - location: scripts/stop_server.sh
       timeout: 300
